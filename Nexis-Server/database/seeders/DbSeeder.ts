c6aaf0a2d189155e0187fead8305caad2ffb9fc2
Kristián Červenka <kristian.cervenka@gmail.com>
2024-11-12 23:53:24 +0100

    :sparkles: Models and migrations done

--- Nexis-Server/database/seeders/DbSeeder.ts
+++ Nexis-Server/database/seeders/DbSeeder.ts
@@ -1,7 +1,14 @@
 import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import Channel from "App/Models/Channel";
 
 export default class extends BaseSeeder {
   public async run () {
-    // Write your database queries inside the run method
+    const uniqueKey = "channel_name";
+
+    await Channel.updateOrCreateMany(uniqueKey, [
+      {
+        channel_name: "general",
+      },
+    ]);
   }
 }
